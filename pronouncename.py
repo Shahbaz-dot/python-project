e51afdadc70b940777669f57436d9bea3f16d27e
Shahbaz Ehfaz <mrshahbaz604@gmail.com>
2025-11-23 19:40:49 +0530

    Update pronouncename.py

--- pronouncename.py
+++ pronouncename.py
@@ -1,11 +1,4 @@
-# Write a program to pronounce lists of name using win32 Ap1
-
-
-# Source - https://stackoverflow.com/a
-# Posted by Charlie
-# Retrieved 2025-11-15, License - CC BY-SA 4.0
-
-# for mac
+# Write a program to pronounce lists of name using import
 
 from os import system
 names = ["Shahbaz", "Zishan", "Wasif", "Kaif", "Mussharaf", "Arshad", "Sunny"]
